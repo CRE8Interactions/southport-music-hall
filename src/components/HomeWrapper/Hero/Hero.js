27c093d901738eb2328930b4c11e2cf168cd312c
Jaime Convery <jaimeconvery@gmail.com>
2024-03-19 11:36:28 -0400

    styyle: fix spacing

--- src/components/HomeWrapper/Hero/Hero.js
+++ src/components/HomeWrapper/Hero/Hero.js
@@ -11,7 +11,7 @@ export default function Hero() {
     return (
         <Stack as="header" gap={2} className={`text-center align-items-center ${styles.hero}`}>
             <h1 className='heading-lg'>Discover the latest <span className='d-flex gap-1'><Badge bg="default" className='badge-primary badge-lg badge--schedule'>upcoming</Badge> events</span></h1>
-            <h2 className="normal-lg fw-medium"><span className='text-info'>from</span> Southport Hall</h2>
+            <h2 className="normal-lg fw-medium m-0"><span className='text-info'>from</span> Southport Hall</h2>
         </Stack>
     );
 }
