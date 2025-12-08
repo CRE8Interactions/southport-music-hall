86f0f512ed161f442d4923901069b5700f243aaf
Jaime Convery <jaimeconvery@gmail.com>
2025-12-08 16:03:05 -0500

    fix: remove cache

--- src/lib/venues.js
+++ src/lib/venues.js
@@ -1,6 +1,8 @@
 export async function getVenue(slug) {
-    // Call an external API endpoint to get venues.
-    const res = await fetch(`${process.env.API}/venues/${slug}/events`, { next: { revalidate: 30 } })
+    // Call an external API endpoint to get venues - no cache - fetch each time
+    const res = await fetch(`${process.env.API}/venues/${slug}/events`, {
+        cache: "no-store",
+    })
     const data = await res.json()
     return data
 }
